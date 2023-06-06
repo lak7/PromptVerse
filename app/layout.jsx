d400e55bfb82cf5070845e0f616a85bc346305c5
lakshay gupta <lakshaygupta2511@gmail.com>
2023-06-06 18:07:24 +0530

    Initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import "@styles/globals.css";
+import { Children } from "react";
+
+export const metadata = {
+    title: "Promptverse",
+    description: "Discover and share AI prompts"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html>
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+
+            <main className="app">
+                {children}
+            </main>
+            
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
